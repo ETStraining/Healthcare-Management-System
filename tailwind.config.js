61faff1899c72f5d27cd8efe1aef1af823054a16
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-16 10:32:25 +0200

    project initialization

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dc-blue": "#396cf0",
+        "mut-blue": "#396cf01a",
+        "muted": "#8492a6",
+        "dark": "#212529",
+      }
+    },
+  },
+  plugins: [],
+}
+
