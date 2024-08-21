e575370852470c3fb17848e3c2290e9867236c8c
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-21 12:51:50 +0200

    Added homecard component into index.dashboard

--- hospital-app/src/pages/dashboard/index.jsx
+++ hospital-app/src/pages/dashboard/index.jsx
@@ -1,13 +1,16 @@
 import HomeCard from "../../components/HomeCard.jsx";
-
+import { ImAidKit as Aid } from "react-icons/im";
+import { RiWheelchairFill as Chair } from "react-icons/ri";
+import { FaHospitalUser as Patient } from "react-icons/fa";
+import { TbReportMoney as Money } from "react-icons/tb";
 const Dashboard = () => {
     return(
         <>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-                <HomeCard />
-                <HomeCard />
-                <HomeCard />
-                <HomeCard />
+                <HomeCard Icon={Aid} title={"Appointments"} count={30} />
+                <HomeCard Icon={Patient} title={"Patients"} count={10} />
+                <HomeCard Icon={Chair} title={"Operations"} count={15} />
+                <HomeCard Icon={Money} title={"Earning"} count={200} />
             </div>
         </>
     )
