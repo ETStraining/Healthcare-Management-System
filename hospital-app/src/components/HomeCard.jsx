fde9341cf4b5574ce93487f5bad58a794f3afe25
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-21 12:50:27 +0200

    Added a home card component

--- hospital-app/src/components/HomeCard.jsx
+++ hospital-app/src/components/HomeCard.jsx
@@ -1,17 +1,24 @@
-import { ImAidKit as Aid } from "react-icons/im";
+import PropTypes from "prop-types";
 
-const HomeCard = () => {
+const HomeCard = ({Icon, title, count}) => {
     return(
         <div className="bg-dc-blue/15 min-h-[88px] py-5 flex items-center gap-7 justify-center px-5 rounded-lg">
             <span className="text-dc-blue text-6xl">
-                <Aid />
+                <Icon />
             </span>
             <div className="flex flex-col gap-2">
-                <h3 className="text-dark font-bold">Appointments</h3>
-                <span className="text-4xl font-bold text-dc-blue">123</span>
+                <h3 className="text-dark font-bold">{title}</h3>
+                <span className="text-4xl font-bold text-dc-blue">{count}</span>
             </div>
         </div>
     )
+}
+
+
+HomeCard.propTypes = {
+    Icon: PropTypes.elementType.isRequired,
+    title: PropTypes.string.isRequired,
+    count: PropTypes.number.isRequired
 }
 
 
