7f2a17946b11ec21c03282eaa69d1a638a49e1f3
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-21 17:02:51 +0200

    Removed some errors and few adjustments added

--- user-app/src/components/forms/BookingForm.jsx
+++ user-app/src/components/forms/BookingForm.jsx
@@ -9,15 +9,21 @@ import { validatePatientName as validatePatientName2} from "../../helpers/second
 
 const BookingForm = () => {
 
-    const [name,setname] = useState (" ")
-    const [email,setemail] = useState (" ")
-    const [Phone,setPhone] = useState (" ")
-    const [Time,setTime] = useState (" ")
-    const [Date,setDate] = useState (" ")
-    const [Comments,setComments] = useState (" ")
+    const initialFormData = {
+        patientName: "",
+        department: "",
+        doctor: "",
+        patientEmail: "",
+        patientPhone: "",
+        date: "",
+        time: "",
+        comment: ""
+    }
+
+    const [formData, setFormData] = useState(initialFormData);
     
 
-    const handlesubmit = (e) => {
+    const handleSubmit = (e) => {
      e.preventDefault();
      const isValid1 = validatePatientName1(name);
      const isValid2 = validatePatientName2(name);
@@ -31,53 +37,64 @@ const BookingForm = () => {
 
     return (
         <div className="form-wrapper">
-            <form onSubmit={handlesubmit}>
+            <form onSubmit={handleSubmit}>
                 <div className="flex flex-col gap-3">
                     <Input
-                        onChange={
-                     setname(e.target.value)
-                        }
-value={
-name
-}
-
+                        onChange={() => {}}
+                        value={formData.patientName}
                         label={"Patient name"}
                         placeholder={"Patient name"}
                         required
                     />
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-                        <Select onChange={() => {
-                        }} label={"Departments"} required>
+                        <Select
+                            onChange={() => {}}
+                            label={"Departments"} required>
                             <Select.Option value={"1"}>Department 1</Select.Option>
                             <Select.Option value={"2"}>Department 2</Select.Option>
                             <Select.Option value={"3"}>Department 3</Select.Option>
                         </Select>
-                        <Select onChange={() => {
-                        }} label={"Doctor"} required>
+                        <Select
+                            onChange={() => {}}
+                            label={"Doctor"} required>
                             <Select.Option value={"1"}>Department 1</Select.Option>
                             <Select.Option value={"2"}>Department 2</Select.Option>
                             <Select.Option value={"3"}>Department 3</Select.Option>
                         </Select>
                     </div>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-                        <Input onChange={
-                             setemail(e.target.value)
-                        }
-                        value={
-                            email
-                            }
-                        
-                        label={"You email"} placeholder={"email@example.com"} required/>
-                        <Input onChange={setPhone(e.target.value)} value={Phone} label={"You Phone"} placeholder={"+250791154300"} required/>
+                        <Input
+                            onChange={() => {}}
+                            value={formData.patientEmail}
+                            label={"You email"}
+                            placeholder={"email@example.com"} required/>
+                        <Input
+                            onChange={() => {}}
+                            value={formData.patientPhone}
+                            label={"You Phone"}
+                            placeholder={"+250791154300"} required/>
                     </div>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-                        <Input onChange={setDate(e.target.value)} value={Date} type={"date"} label={"Date"} placeholder={"date"} required/>
-                        <Input onChange={setTime(e.target.value)}  value={Time} type={"time"} label={"Time"} placeholder={"03:30AM"} required/>
+                        <Input
+                            onChange={() => {}}
+                            type={"date"}
+                            value={formData.date}
+                            label={"Date"}
+                            placeholder={"date"} required/>
+                        <Input
+                            onChange={() => {}}
+                            type={"time"}
+                            value={formData.time}
+                            label={"Time"}
+                            placeholder={"03:30AM"} required/>
                     </div>
-                    <Textarea onChange={ setComments(e.target.value)} value={Comments} label={"Comments"} placeholder={"Problem description:"}/>
+                    <Textarea
+                        onChange={() => {}}
+                        value={formData.comment}
+                        label={"Comments"}
+                        placeholder={"Problem description:"}/>
 
-                    <Button onClick={() => {
-                    }}>
+                    <Button onClick={() => {}}>
                         Book an Appointment
                     </Button>
                 </div>
