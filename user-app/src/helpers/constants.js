69a47cc739269de55b455ab82cf794b4ddfe8618
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-23 13:07:05 +0200

    Added contants.js

---
+++ user-app/src/helpers/constants.js
@@ -0,0 +1,22 @@
+export const links = [
+    {
+        path: "/",
+        label: "home"
+    },
+    {
+        path: "/about",
+        label: "about us"
+    },
+    {
+        path: "/doctors",
+        label: "doctors"
+    },
+    {
+        path: "/departments",
+        label: "departments"
+    },
+    {
+        path: "/contact",
+        label: "contact"
+    }
+]
