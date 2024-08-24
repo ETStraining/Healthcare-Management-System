e787068deda85e01e0a5c5ebff6326e14ccda214
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-24 21:21:44 +0200

    Added form to add patients

--- hospital-app/src/pages/dashboard/Patients.jsx
+++ hospital-app/src/pages/dashboard/Patients.jsx
@@ -1,14 +1,30 @@
 import Button from "../../components/ui/Button.jsx";
 import InnerHeader from "../../components/layout/InnerHeader.jsx";
+import {useState} from "react";
+import Dialog from "../../components/ui/Dialog.jsx";
+import AddPatient from "../../components/forms/AddPatient.jsx";
+import THead from "../../components/table/THead.jsx";
 
 const Patients = () => {
+    const headers = ["Names", "Phone", "Province", "District", "Village", "Cell"];
+    const [dialogOpen, setDialogOpen] = useState(false)
+
     return(
         <>
+            {
+                dialogOpen &&
+                <Dialog handleClose={() => setDialogOpen(false)}>
+                    <Dialog.Title>Add Patient</Dialog.Title>
+                    <AddPatient />
+                </Dialog>
+            }
             <InnerHeader title={"Patients"} className="flex items-center justify-between">
-                <Button onClick={() => {}}>Add a Patient</Button>
+                <Button onClick={() => setDialogOpen(true)}>Add a Patient</Button>
             </InnerHeader>
             <div className="relative overflow-x-auto">
-
+                <table>
+                    <THead headers={headers} />
+                </table>
             </div>
         </>
     )
