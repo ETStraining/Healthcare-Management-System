0f168357faaa6ac92f6f5841f794868d66f22405
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-07 20:14:38 +0200

    refactor(Button): Added some adjustments like variant accepting

--- hospital-app/src/components/ui/Button.jsx
+++ hospital-app/src/components/ui/Button.jsx
@@ -1,11 +1,19 @@
 import PropTypes from "prop-types";
 
-function Button ({children, disabled = false, onClick, className}) {
+function Button ({children, disabled = false, onClick, className, variant="primary"}) {
+    const variantStyles = (variable) => {
+        switch (variable) {
+            case "primary":
+                return `${disabled ? `bg-dc-blue/70` : `bg-dc-blue`}`
+            case "danger":
+                return `${disabled ? `bg-red-200` : `bg-red-400`}`
+        }
+    }
     return(
         <button
             disabled={disabled}
             onClick={onClick}
-            className={`${className}  px-5 py-2 text-white rounded-md ${disabled ? `cursor-not-allowed bg-dc-blue/70` : `cursor-pointer bg-dc-blue`}`}
+            className={`${className} ${variantStyles(variant)}  px-5 py-2 text-white rounded-md ${disabled ? `cursor-not-allowed` : `cursor-pointer`}`}
         >
             {children}
         </button>
@@ -17,6 +25,7 @@ Button.propTypes = {
     disabled: PropTypes.bool,
     onClick: PropTypes.func.isRequired,
     className: PropTypes.string,
+    variant: PropTypes.string
 }
 
 export default Button;
