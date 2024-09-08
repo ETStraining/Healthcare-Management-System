ddc533aa5cc75a62d73435d1c63660c066a7437d
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-08 19:52:49 +0200

    chore(dependencies): Added react-toastify

--- user-app/src/main.jsx
+++ user-app/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ToastContainer} from "react-toastify";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+        <ToastContainer />
+        <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
