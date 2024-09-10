151ab732c25764287a512514bd39e4788e8d6e35
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-10 21:18:30 +0200

    refactor: Added methods and hooks to approve and reject appointments

--- hospital-app/src/hooks/useAppointments.js
+++ hospital-app/src/hooks/useAppointments.js
@@ -1,9 +1,30 @@
-import {useQuery} from "@tanstack/react-query";
-import {fetchAppointments} from "../api/appointmentAPI.js";
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+import {approveAppointment, fetchAppointments, rejectAppointment} from "../api/appointmentAPI.js";
 
 export const useAppointments = (filters = {}, page = 1, limit = 10) => {
     return useQuery({
         queryKey: ["appointments", filters, page, limit],
         queryFn: () => fetchAppointments(filters, page, limit)
+    })
+}
+
+export const useApproving = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: ({appointmentId, formData}) => approveAppointment(appointmentId, formData),
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["appointments"]})
+        }
+    })
+}
+
+
+export const useRejecting = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: ({appointmentId, formData}) => rejectAppointment(appointmentId, formData),
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["appointments"]})
+        }
     })
 }
