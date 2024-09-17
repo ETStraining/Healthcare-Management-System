0c295f7aa156f6d401519e987a86bfce7e126f3f
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-17 13:43:17 +0200

    refactor(ui/components): added cn and made the component clean

--- user-app/src/components/ui/Input.jsx
+++ user-app/src/components/ui/Input.jsx
@@ -1,36 +1,30 @@
 import PropTypes from "prop-types";
+import React from "react";
+import {cn} from "../../utils/utils.js";
 
-const Input = ({ label, type = "text", placeholder, value, onChange, className, required, ...rest }) => {
-    return (
-        <label>
-            <span className="text-dark font-medium">{label}</span>
-            {required && <span className="ms-1 text-red-400 font-medium">*</span>}
+const Input = React.forwardRef(({ label, type = "text", className, required, ...props }, ref) => (
+    <label>
+        <span className="text-dark font-medium">{label}</span>
+        {required && <span className="ms-1 text-red-400 font-medium">*</span>}
 
-            <input
-                type={type}
-                value={value}
-                onChange={onChange}
-                required={required}
-                placeholder={placeholder}
-                {...rest}
-                className={`${className} mt-1 py-[0.375rem] px-3 w-full outline-none border border-light-gray rounded-[0.375rem] text-dark text-[0.9375rem] transition-all focus:border-dc-blue focus:ring-4 focus:ring-dc-blue/30`}
-            />
-        </label>
-    );
-};
+        <input
+            ref={ref}
+            type={type}
+            required={required}
+            {...props}
+            className={cn('mt-1 py-[0.375rem] px-3 w-full outline-none border border-light-gray rounded-[0.375rem] text-dark text-[0.9375rem] transition-all focus:border-dc-blue focus:ring-4 focus:ring-dc-blue/30', className)}
+        />
+    </label>
+))
+
+Input.displayName = "Input"
 
 // PropTypes validation for the Input component
 Input.propTypes = {
-    label: PropTypes.string.isRequired,            // Label text is required
-    type: PropTypes.string,                        // Type of the input field (e.g., text, password)
-    placeholder: PropTypes.string.isRequired,      // Placeholder text is required
-    value: PropTypes.oneOfType([                   // Value can be a string or a number
-        PropTypes.string,
-        PropTypes.number
-    ]),
-    onChange: PropTypes.func.isRequired,           // onChange function is required for handling input changes
-    className: PropTypes.string,                   // Optional additional classes for styling
-    required: PropTypes.bool                       // Determines if the field is required, default is false
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    className: PropTypes.string,
+    required: PropTypes.bool
 };
 
 
