fd41b1774998cfdc0e2a84222831de361b35beae
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-17 13:42:12 +0200

    refactor(ui/components) added cva and fixed css classes correclty

--- user-app/src/components/ui/Button.jsx
+++ user-app/src/components/ui/Button.jsx
@@ -1,22 +1,41 @@
 import PropTypes from "prop-types";
+import {cn} from "../../utils/utils.js";
+import {cva} from "class-variance-authority";
+import React from "react";
 
-function Button ({children, disabled = false, onClick, className}) {
-    return(
-        <button
-            disabled={disabled}
-            onClick={onClick}
-            className={`${className}  px-5 py-2 text-white rounded-md ${disabled ? `cursor-not-allowed bg-dc-blue/70` : `cursor-pointer bg-dc-blue`}`}
-        >
-            {children}
-        </button>
-    )
-}
+const buttonVariants = cva(
+    "inline-block px-5 py-2 text-white rounded-md cursor-pointer transition-all",
+    {
+        variants: {
+            variant: {
+                primary: "bg-dc-blue/80 hover:bg-dc-blue",
+                destructive: "bg-red-200 text-red-700 hover:bg-red-300"
+            },
+            disabled: {
+                true: "cursor-not-allowed bg-dc-blue/70",
+                false: ""
+            }
+        },
+        defaultVariants: {
+            variant: "primary"
+        }
+    }
+)
+
+const Button = React.forwardRef(({className, variant, disabled, ...props}, ref) => (
+    <button
+        ref={ref}
+        className={cn(buttonVariants({variant, disabled: !!disabled}), className)}
+        {...props}
+    />
+))
+
+Button.displayName = "Button"
 
 Button.propTypes = {
-    children: PropTypes.node.isRequired,
-    disabled: PropTypes.bool,
-    onClick: PropTypes.func,
+    variant: PropTypes.string,
     className: PropTypes.string,
+    disabled: PropTypes.bool
 }
 
 export default Button;
